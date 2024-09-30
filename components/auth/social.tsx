e82e5465f687c2ee4e28aba967fe082eb30f5c41
Srivatsan <sriram.srini12@gmail.com>
2024-09-30 19:32:53 +0530

    Added callback for login

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,14 +3,18 @@ import { signIn } from 'next-auth/react'
 import { FcGoogle } from "react-icons/fc"
 import { Button } from "../ui/button"
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
+import { useSearchParams } from 'next/navigation'
 
 export const Social = () => {
+
+    const searchParams = useSearchParams()
+    const callbackUrl = searchParams.get("callbackUrl")
 
     const onClick = (provider: "google") => {
         console.log("Im here")
         try {
             signIn(provider, {
-                callbackUrl: DEFAULT_LOGIN_REDIRECT
+                callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT
             })
         } catch(error) {
             console.log(error)
