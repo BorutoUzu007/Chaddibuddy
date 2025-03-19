271e9160f1f6f8870c42d06f7c9f3340772275e6
Srivatsan <sriram.srini12@gmail.com>
2025-03-19 13:08:45 +0530

    added notFound page

--- app/(platform)/user/[userId]/page.tsx
+++ app/(platform)/user/[userId]/page.tsx
@@ -1,13 +1,17 @@
 import { ProfileSectionAvatar } from "@/components/platform/profile-page/profile-section-avatar"
 import { getUserById } from "@/utils/user"
+import { notFound } from "next/navigation"
 
 export default async function UserProfile ({params}: {params: {userId: string}}) {
     const user = await getUserById(params.userId)
+    if (!user) {
+        notFound()
+    }
     return (
         <div className="flex justify-center pt-10">
             <div className="w-full sm:w-[850px] mx-2 max-w-[850px] h-auto">
                 <div className="flex flex-col items-center pt-10">
-                    <ProfileSectionAvatar profileImage={user?.image || ""}  />
+                    <ProfileSectionAvatar profileImage={user?.image || ""} />
                     <p className="text-white"> Hello good morning</p>
                 </div>
             </div>
