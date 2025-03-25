1b4b2f4d8ce529efcd0640d8b1cd36c26765634a
Srivatsan <sriram.srini12@gmail.com>
2025-03-25 20:09:33 +0530

    migration changes test

---
+++ drizzle/0006_soft_human_fly.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "user" DROP COLUMN IF EXISTS "daily_task_outline";--> statement-breakpoint
+ALTER TABLE "user" DROP COLUMN IF EXISTS "weekly_task_outline";--> statement-breakpoint
+ALTER TABLE "user" DROP COLUMN IF EXISTS "monthly_task_outline";--> statement-breakpoint
+ALTER TABLE "user" DROP COLUMN IF EXISTS "yearly_task_outline";--> statement-breakpoint
+ALTER TABLE "user" DROP COLUMN IF EXISTS "custom_task_outline";
